f0a726474816569bc9f853f3d3e08ba83031c76b
CJ <thecjhorton@gmail.com>
2018-07-09 18:49:51 -0500

    remove auth controller/views

--- lib/revelry-koans_web/router.ex
+++ lib/revelry-koans_web/router.ex
@@ -1,6 +1,6 @@
 defmodule RevelryKoansWeb.Router do
   use RevelryKoansWeb, :router
-  alias RevelryKoansWeb.{RequireAuth, LoadUser}
+  alias RevelryKoansWeb
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -8,11 +8,6 @@ defmodule RevelryKoansWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-    plug LoadUser
-  end
-
-  pipeline :require_auth do
-    plug RequireAuth
   end
 
   pipeline :api do
@@ -23,14 +18,6 @@ defmodule RevelryKoansWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
-    get "/sessions/new", SessionController, :new
-    post "/sessions/new", SessionController, :create
-  end
-
-  scope "/", RevelryKoansWeb do
-    pipe_through [:browser, :require_auth]
-
-    get "/sessions/delete", SessionController, :delete
   end
 
   # Other scopes may use custom stacks.
